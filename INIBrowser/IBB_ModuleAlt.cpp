ba56a31c9c9345a11f726ada7045d7484207cb32
Ironhammer Std <2482911962@qq.com>
2026-07-03 20:25:36 +0800

    Sort Modules By Name

--- INIBrowser/IBB_ModuleAlt.cpp
+++ INIBrowser/IBB_ModuleAlt.cpp
@@ -23,6 +23,8 @@
 
 #pragma comment(lib, "crypt32.lib")
 
+bool StrCmpZHCN(const std::string& l, const std::string& r);
+
 // 将二进制数据转换为 Base64 字符串
 std::string DataToBase64(const std::vector<BYTE>& Data) {
     DWORD dwLength = 0;
@@ -1664,6 +1666,7 @@ namespace IBB_ModuleAltDefault
         std::string _TEXT_UTF8 Name;
         std::vector<std::unique_ptr<ModuleTree>> Sub;
         std::unordered_map<std::string, IBB_ModuleAlt*> Modules;
+        std::vector<std::string> ModuleOrder;
         bool ChildMenuHovered{ false };
         bool LastHovered{ false };
 
@@ -1737,8 +1740,9 @@ namespace IBB_ModuleAltDefault
                 else DrawFolderIcon(Pos, (float)FontHeight);
                 S->LastHovered = Hovered;
             }
-            for (auto& [N, M] : Modules)
+            for (auto& name : ModuleOrder)
             {
+                auto M = Modules.at(name);
                 SearchModuleAlt::RenderModuleAltSelect(M);
             }
         }
@@ -1783,6 +1787,9 @@ namespace IBB_ModuleAltDefault
                     NewModule(std::move(Mod));
                 }
             }
+            ModuleOrder = Modules | std::views::keys | std::ranges::to<std::vector>();
+            std::ranges::sort(Sub, StrCmpZHCN, [](const auto& Tree) {return Tree->Name; });
+            std::ranges::sort(ModuleOrder, StrCmpZHCN, [this](const auto& str) { return Modules[str]->DescShort; });
         }
     };
     std::unordered_map<std::string, IBB_ModuleAlt> ArtModules;
@@ -1931,8 +1938,9 @@ namespace IBB_ModuleAltDefault
                     ImGui::TreePop();
                 }
             }
-            for (auto& [name, mod] : tree.Modules)
+            for (auto& name: tree.ModuleOrder)
             {
+                auto mod = tree.Modules.at(name);
                 if (!mod) continue;
                 auto label = mod->DescShort.empty() ? name : mod->DescShort;
                 ImGui::Selectable(label.c_str());
